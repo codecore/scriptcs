a25d52f2389c1dc5b9bfa5ff289622d0f52eb1ba
Scott Slack <kd7vbs@gmail.com>
2013-03-16 20:22:30 -0700

    Added support for the Verify object
    A low-noise code-contract-like object for parameter verification. Think
    Debug.Assert(null != o);

---
+++ Utilities/Verify.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text; 
+using System.Threading.Tasks;
+using System.Diagnostics;
+namespace Utilities
+{
+    public static class Verify
+    {
+        public static void Parameters(string s)
+        { Debug.Assert(!String.IsNullOrEmpty(s)); }
+        public static void Parameters(string s1, string s2)
+        { Debug.Assert(!String.IsNullOrEmpty(s1) && !String.IsNullOrEmpty(s2)); }
+        public static void Parameters(object o)
+        { Debug.Assert(null != o); }
+    }
+}
--- src/ScriptCs.Core/FileSystem.cs
+++ src/ScriptCs.Core/FileSystem.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using Utilities;
 
 namespace ScriptCs
 {
@@ -8,36 +9,43 @@ namespace ScriptCs
     {
         public IEnumerable<string> EnumerateFiles(string dir, string searchPattern)
         {
+            Verify.Parameters(dir, searchPattern);
             return Directory.EnumerateFiles(dir, searchPattern, SearchOption.AllDirectories);
         }
 
         public void Copy(string source, string dest, bool overwrite)
         {
+            Verify.Parameters(source, dest);
             File.Copy(source, dest, overwrite);
         }
 
         public bool DirectoryExists(string path)
         {
+            Verify.Parameters(path);
             return Directory.Exists(path);
         }
 
         public void CreateDirectory(string path)
         {
+            Verify.Parameters(path);
             Directory.CreateDirectory(path);
         }
 
         public string ReadFile(string path)
         {
+            Verify.Parameters(path);
             return File.ReadAllText(path);
         }
 
         public string[] ReadFileLines(string path)
         {
+            Verify.Parameters(path);
             return File.ReadAllLines(path);
         }
 
         public bool IsPathRooted(string path)
         {
+            Verify.Parameters(path);
             return Path.IsPathRooted(path);
         }
 
@@ -53,26 +61,31 @@ namespace ScriptCs
 
         public DateTime GetLastWriteTime(string file)
         {
+            Verify.Parameters(file);
             return File.GetLastWriteTime(file);
         }
 
         public void Move(string source, string dest)
         {
+            Verify.Parameters(source, dest);
             File.Move(source, dest);
         }
 
         public bool FileExists(string path)
         {
+            Verify.Parameters(path);
             return File.Exists(path);
         }
 
         public Stream CreateFileStream(string filePath, FileMode mode)
         {
+            Verify.Parameters(filePath);
             return new FileStream(filePath, mode);
         }
 
         public string GetWorkingDirectory(string path)
         {
+            Verify.Parameters(path);
             return IsPathRooted(path) ? Path.GetDirectoryName(path) : CurrentDirectory;
         }
     }
